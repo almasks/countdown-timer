b69b0420abb343c896d7c530224ddf4a7c229eec
Almas K S <mailmealmasks@gmail.com>
2023-06-15 19:42:52 +0530

    countdown enabled

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const months =[
+const months = [
     "January",
     "February",
     "March",
@@ -9,11 +9,11 @@ const months =[
     "August",
     "September",
     "October",
-    "November", 
+    "November",
     "December"
 
 ]
-const weekdays= [
+const weekdays = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -22,18 +22,48 @@ const weekdays= [
     "Friday",
     "Saturday",
 ]
-const giveAway =document.querySelector(".giveaway")
-const deadLine =document.querySelectorAll(".deadline-format")
 
-let futureDate = new Date (2023,6,23,11,30,0)
+const giveAway = document.querySelector(".giveaway")
+const deadLine = document.querySelector(".deadline")
+const items = document.querySelectorAll(".deadline-format h4")
+
+let futureDate = new Date(2023, 5, 23, 11, 30, 0)
+
 const year = futureDate.getFullYear()
 const month = months[futureDate.getMonth()]
-const date =futureDate.getDate()
-const day =weekdays[futureDate.getDay()]
-const hour =futureDate.getHours()
-const minute =futureDate.getMinutes()
+const date = futureDate.getDate()
+const day = weekdays[futureDate.getDay()]
+const hour = futureDate.getHours()
+const minute = futureDate.getMinutes()
 giveAway.textContent = `Giveaway Ends On ${day}, ${date} ${month} ${year},${hour}:${minute} am`
-let futureTime = futureDate.getTime()
-let today = new Date().getTime()
- let t = futureTime-today
- console.log(t)
+
+function getRemainingTime() {
+    let futureTime = futureDate.getTime();
+    let today = new Date().getTime();
+    let t = futureTime - today;
+// milliseconds
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    let days = Math.floor(t / oneDay);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+;   let seconds = Math.floor((t % oneMinute) / 1000);
+    const values = [days, hours, minutes, seconds];
+    function format(item) {
+        if (item < 10) {
+            return (item = `0${item}`)
+        }
+        return item
+    }
+    items.forEach(function (item, index) {
+        item.innerHTML = format(values[index]);
+    })
+    if (t < 0) {
+        clearInterval(contdown);
+        deadLine.innerHTML = `<h4 class="expired"> Sorry, this giveaway has expired </h4>`
+    }
+}
+let contdown = setInterval(getRemainingTime, 1000)
+getRemainingTime()
